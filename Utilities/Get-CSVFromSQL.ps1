a2fbe1ebaec16623937eb82df175981aafab2673
Mark Strendin <mark.strendin@lskysd.ca>
2024-03-18 14:38:33 -0600

    Enhance logging and reporting for both the IE EduForms script, and the SQL script. Add teams webhook capabilities for easier error detection.

--- Utilities/Get-CSVFromSQL.ps1
+++ Utilities/Get-CSVFromSQL.ps1
@@ -1,17 +1,30 @@
 param (
     [Parameter(Mandatory=$true)][string]$ConfigFile,
     [Parameter(Mandatory=$true)][string]$SQLQueryBase64,
-    [Parameter(Mandatory=$true)][string]$OutputFile
+    [Parameter(Mandatory=$true)][string]$OutputFile,
+    [Parameter(Mandatory=$true)][string]$LogFile
 )
 
+Function LogThis
+{
+   Param ([string]$logmessage)
+   $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+   $Line = "$Stamp $logmessage"
+   Add-content $Logfile -value $Line
+}
+
+LogThis "Starting Get-CSVFromSQL.ps1"
+
+LogThis "SQL Query: $SQLQueryBase64"
+
 $SQLQuery = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($SQLQueryBase64))
-#write-host $SQLQuery
 
 # #################################################
 # Ensure that necessary folders exist
 # #################################################
 
 $ActualConfigFilePath = $(Resolve-Path $ConfigFile)
+LogThis "Loading configuration file from $ActualConfigFilePath"
 
 # #################################################
 # Load config file
@@ -23,11 +36,11 @@ if ((Test-Path -Path $ActualConfigFilePath) -eq $false) {
 $configXML = [xml](Get-Content $ActualConfigFilePath)
 $SQLConnectionString = $configXml.Settings.MySchoolSask.ReportingDatabaseConnectionString
 
-
 if ((Test-Path -Path $OutputFile) -eq $true) {
     Remove-Item $OutputFile
 }
 
+LogThis "Config file loaded.";
 
 # #################################################
 # Run SQL query
@@ -40,7 +53,10 @@ $dataset = New-Object System.Data.DataSet
 
 $retryCount = 0
 $maxRetries = 5
-$retryDelay = 30
+$retryDelay = 60
+
+LogThis "Max retries is $maxRetries. Retry delay is $retryDelay seconds."
+LogThis "Running query..."
 
 while ($retryCount -lt $maxRetries) {
     try {
@@ -49,12 +65,16 @@ while ($retryCount -lt $maxRetries) {
     } catch {
         $retryCount++
         if ($retryCount -eq $maxRetries) {
-            throw "Failed to execute SQL query after $maxRetries attempts."
+            LogThis "Failed to execute SQL query after $maxRetries attempts."
         }
-        Write-Host "Exception occurred. Retrying in $retryDelay seconds..."
+        LogThis "Exception occurred."
+        LogThis $_
+        LogThis "Retrying in $retryDelay seconds..."
         Start-Sleep -Seconds $retryDelay
     }
-}
+} 
+
+LogThis "Finished running query."
 
 # #################################################
 # Process the query results
@@ -66,9 +86,18 @@ while ($retryCount -lt $maxRetries) {
 # Export results to CSV file
 # #################################################
 
+LogThis "Exporting results to $OutputFile"
+
 if ($($dataset.Tables.Count) -gt 0) {
     $dataset.Tables[0] | Export-Csv -Path $OutputFile -NoTypeInformation
 }
+
+$OutFileHash = Get-FileHash $OutputFile -Algorithm SHA256
+$OutFileSize = (Get-Item $OutputFile).Length
+LogThis "SHA256 hash of $OutputFile is $($OutFileHash.Hash)"
+LogThis "Size of $OutputFile is $OutFileSize bytes"
+
+LogThis "Done running Get-CSVFromSQL.ps1."
 
 
 
